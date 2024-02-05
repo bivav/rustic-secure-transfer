7ee944adcc0846988ace3458ad741cdc208fe7e9
bivav <bivavraj@gmail.com>
2024-02-05 23:54:01 +0000

    Metadata transfer check completed using Tokio TcpStream

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,135 @@
+use std::fs;
+use std::path::Path;
+
+use anyhow::{Context, Result};
+use clap::ArgMatches;
+use serde::{Deserialize, Serialize};
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 pub struct Config {
-    pub query: String,
-    pub filename: String,
-    pub ip_address: String,
+    pub mode: String,
+    pub file_path: String,
+    pub file_name: String,
+    pub destination: String,  // Can be IP:Port for send mode or Port for get mode
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 4 {
-            return Err("not enough arguments");
+    pub fn from_matches(matches: &ArgMatches) -> Config {
+        let mode = matches.get_one::<String>("mode").unwrap().to_string();
+
+        let file_path = matches.get_one::<String>("file").unwrap().to_string();
+
+        let path = Path::new(&file_path);
+        let full_path = fs::canonicalize(path).unwrap().display().to_string();
+
+        let file_name = path.file_name().unwrap().to_string_lossy();
+        // let (_, file_name) = file_path.split_at(file_path.rfind("/").unwrap() + 1);
+
+        let destination = matches.get_one::<String>("destination").unwrap().to_string();
+
+        Config {
+            mode,
+            file_path: full_path,
+            file_name: file_name.to_string(),
+            destination,
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        let ip_address = args[3].clone();
-        Ok(Config { query, filename, ip_address })
+    }
+}
+
+pub struct SecureTransfer {
+    pub path: String,
+}
+
+impl SecureTransfer {
+    pub fn new(path: &str) -> SecureTransfer {
+        SecureTransfer {
+            path: path.to_string(),
+        }
+    }
+
+    pub async fn read_file_async(&self) -> Result<Vec<u8>> {
+        let mut file = File::open(&self.path)
+            .await
+            .context("Failed to open file")?;
+        let mut content = vec![];
+        file.read_to_end(&mut content)
+            .await
+            .context("Failed to read file contents")?;
+        Ok(content)
+    }
+
+    pub async fn write_file_async(&self, data: Vec<u8>) -> Result<()> {
+        let mut file = File::create(&self.path)
+            .await
+            .context("Failed to create file")?;
+        file.write_all(&data)
+            .await
+            .context("Failed to write file contents")?;
+        Ok(())
+    }
+
+    pub async fn connect_to_client(address: &str) -> Result<TcpStream> {
+        println!("Connecting to client at {}", address);
+        let stream = TcpStream::connect(format!("{}:{}", address, "8080")).await?;
+        Ok(stream)
+    }
+
+    pub async fn start_receiving(port: u16) -> Result<()> {
+        let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
+
+        loop {
+            let (mut socket, addr) = listener.accept().await?;
+            println!("Accepted connection from: {}", addr);
+            tokio::spawn(async move {
+                let mut buffer = [0; 1024];
+                match socket.read(&mut buffer).await {
+                    Ok(size) => {
+                        println!("Received {} bytes: {:?}", size, &buffer[..size]);
+
+                        // Check if there's more than just the length bytes
+                        if size > 8 {
+                            // Skip the first 8 bytes and deserialize
+                            match serde_json::from_slice::<FileMetadata>(&buffer[8..size]) {
+                                Ok(metadata) => {
+                                    println!("Received metadata: File name: '{}', File size: {} bytes", metadata.file_name, metadata.file_size);
+                                }
+                                Err(e) => {
+                                    println!("Failed to deserialize metadata: {}", e);
+                                }
+                            }
+                        } else {
+                            println!("Not enough data to deserialize metadata");
+                        }
+                    }
+                    Err(e) => {
+                        println!("Failed to receive data: {}", e);
+                    }
+                }
+            });
+        }
+    }
+
+    pub async fn send_metadata(stream: &mut TcpStream, metadata: &str) -> Result<()> {
+        let metadata_length = metadata.len() as u64;
+        stream.write_all(&metadata_length.to_be_bytes()).await.context("Failed to write metadata length")?;
+        stream.write_all(&metadata.as_bytes()).await.context("Failed to write metadata")?;
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FileMetadata {
+    pub file_name: String,
+    pub file_size: u64,
+}
+
+impl FileMetadata {
+    pub fn new(file_name: &str, file_size: u64) -> FileMetadata {
+        FileMetadata {
+            file_name: file_name.to_string(),
+            file_size,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,68 @@
 use std::error::Error;
-use rustic_secure_transfer::Config;
+
+use anyhow::Result;
+use clap::{Arg, Command};
+
+use rustic_secure_transfer::{Config, FileMetadata, SecureTransfer};
+
+const VERSION: &str = "0.1.0";
+const APP_NAME: &str = "Rustic Secure Transfer";
+const APP_ABOUT: &str = "Transfers files securely";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("File Transfer App Started");
 
-    let args = std::env::args().collect::<Vec<String>>();
-    let config = Config::new(&args)?;
+    // let args = std::env::args().collect::<Vec<String>>();
 
-    println!("Query: {}\nFilename: {}\nIP address: {} ", config.query, config.filename, config.ip_address);
+    let args = Command::new(APP_NAME)
+        .version(VERSION)
+        .about(APP_ABOUT)
+        .arg(Arg::new("mode")
+            .help("Mode: 'get' for receiving or 'send' for sending a file")
+            .required(true)
+            .index(1))
+        .arg(Arg::new("file")
+            .help("The file path to send")
+            .required_if_eq("mode", "send")
+            .index(2))
+        .arg(Arg::new("destination")
+            .help("The IP address with port to send to or port to listen on")
+            .required(true)
+            .index(3))
+        .get_matches();
+
+    let config = Config::from_matches(&args);
+
+    match config.mode.as_str() {
+        "get" => {
+            println!("Get file: {}", &config.file_path);
+            let port = 8080;
+            SecureTransfer::start_receiving(port).await?;
+        }
+        "send" => {
+            println!("File to send: {}", &config.file_path);
+            println!("File name: {}", &config.file_name);
+
+            let transfer = SecureTransfer::new(&config.file_path);
+            let file_content = transfer.read_file_async().await?;
+
+            let metadata = FileMetadata {
+                file_name: config.file_name.clone(),
+                file_size: file_content.len() as u64,
+            };
+
+            let serialized_metadata = serde_json::to_string(&metadata)?;
+
+            let mut stream = SecureTransfer::connect_to_client(&config.destination).await?;
+            SecureTransfer::send_metadata(&mut stream, &serialized_metadata).await?;
+
+            println!("File content length: {}", file_content.len());
+        }
+        _ => println!("Invalid mode selected"),
+    }
+
+    // println!("Query: {}\nFilename: {}\nIP address: {} ", &config.query, &config.file_path, &config.ip_address);
 
     Ok(())
 }
