74d53733b308c78e4275dffcfecea9218f0098f0
bivav <bivavraj@gmail.com>
2024-02-07 02:05:33 +0000

    ACK receiving method implemented | Testing code runtime for expensive operation to optimize

--- src/secure_transfer.rs
+++ src/secure_transfer.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::time::Instant;
 use uuid::Uuid;
 
 use rustic_secure_transfer::Config;
@@ -42,22 +43,35 @@ impl FileMetadata {
 
 impl SecureTransfer {
     pub async fn stream_file_and_compute_hash(mut file: File, transaction_id: String, stream: &mut TcpStream) -> Result<()> {
+        let start = Instant::now();
+
         let mut buffer = [0; 64000];
         let mut hash_context = digest::Context::new(&SHA256);
 
         let start_file = b"--START OF FILE--";
         let end_file = b"--END OF FILE--";
+        let end_hash = b"--END OF HASH--";
 
         stream.write_all(start_file).await.context("Failed to send start delimiter")?;
+        stream.flush().await?;
+
+        // let elapsed = start.elapsed();
+        println!("Time elapsed before loop: {:.8} seconds", start.elapsed().as_secs_f64());
 
         loop {
             let bytes = file.read(&mut buffer).await.context("Failed to read file content")?;
             if bytes == 0 { break; }
             stream.write_all(&buffer[..bytes]).await.context("Failed to send file chunk")?;
+            stream.flush().await?;
             hash_context.update(&buffer[..bytes]);
         }
 
+        println!("Time elapsed after loop: {:.8} seconds", start.elapsed().as_secs_f64());
+
         stream.write_all(end_file).await.context("Failed to send EOF delimiter")?;
+        stream.flush().await?;
+
+        println!("Time elapsed after EOF: {:.8} seconds", start.elapsed().as_secs_f64());
 
         println!("File content sent with transaction ID: {}", transaction_id);
 
@@ -68,19 +82,34 @@ impl SecureTransfer {
 
         let serialized = serde_json::to_string(&hash_message)?;
         stream.write_all(serialized.as_bytes()).await.context("Failed to send hash message")?;
+        stream.flush().await?;
 
-        println!("File hash: {:?}", hex::encode(hash_message.hash));
+        stream.write_all(end_hash).await.context("Failed to send hash EOF delimiter")?;
+        stream.flush().await?;
+
+        println!("Time elapsed after hash sent: {:.8} seconds", start.elapsed().as_secs_f64());
+
+        println!("File sent!\nHash generated: {:?}", hex::encode(hash_message.hash));
+
+        println!("Waiting for acknowledgment...");
+        if let Err(err) = Self::wait_for_acknowledgment(stream).await {
+            println!("Error waiting for acknowledgment: {}", err);
+        }
+        println!("Time elapsed after ACK: {:.8} seconds", start.elapsed().as_secs_f64());
 
         Ok(())
     }
 
     pub async fn write_file_async(file_name: String, data: Vec<u8>) -> Result<()> {
-        let mut file = File::create(file_name)
+        println!("Writing file: {}", file_name);
+        let mut file = File::create(file_name.clone())
             .await
             .context("Failed to create file")?;
         file.write_all(&data)
             .await
             .context("Failed to write file contents")?;
+        file.sync_all().await.context("Failed to sync file")?;
+        println!("File written: {}", file_name);
         Ok(())
     }
 
@@ -93,57 +122,72 @@ impl SecureTransfer {
     pub async fn start_receiving(port: u16) -> Result<()> {
         let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
 
+        let start = Instant::now();
+
         loop {
             let (mut socket, addr) = listener.accept().await?;
             println!("Connection accepted from: {}", addr);
             tokio::spawn(async move {
                 let mut overall_buffer = Vec::new();
 
+                println!("Receiving data...");
+                println!("Time elapsed before getting overall buffer: {:.8} seconds", start.elapsed().as_secs_f64());
+
                 loop {
-                    let mut buffer = [0; 1024];
+                    let mut buffer = [0; 4096];
                     if let Ok(size) = socket.read(&mut buffer).await {
-                        if size == 0 {
-                            break; // All data received
-                        }
+                        let delimiter = b"--END OF HASH--";
+                        let delimiter_len = delimiter.len();
+
                         overall_buffer.extend_from_slice(&buffer[..size]);
+
+                        let found_delimiter = buffer[..size].windows(delimiter_len).any(|window| window == delimiter);
+
+                        if found_delimiter {
+                            break;
+                        }
                     } else {
+                        println!("Error reading data.");
                         break;
                     }
                 }
+                println!("Time elapsed AFTER overall buffer: {:.8} seconds", start.elapsed().as_secs_f64());
+                println!("Finished reading data...");
 
                 println!("Received {} bytes.", overall_buffer.len());
 
                 if let Ok(metadata) = SecureTransfer::extract_metadata(&overall_buffer) {
                     println!("Received metadata: {:?}", metadata);
-                    if let Err(e) = Self::send_acknowledgment(&mut socket, "Metadata received.").await {
-                        println!("Error sending acknowledgment: {}", e);
-                    }
 
+                    println!("Time elapsed start & end index for delimiter: {:.8} seconds", start.elapsed().as_secs_f64());
                     let start_delimiter = b"--START OF FILE--";
                     let end_delimiter = b"--END OF FILE--";
+                    let end_hash = b"--END OF HASH--";
                     let start_delimiter_length = start_delimiter.len();
                     let end_delimiter_length = end_delimiter.len();
+                    let end_hash_length = end_hash.len();
 
                     if let Some(start_index) = overall_buffer.windows(start_delimiter_length).position(|window| window == start_delimiter) {
                         if let Some(end_index) = overall_buffer.windows(end_delimiter_length).position(|window| window == end_delimiter) {
+
+                            println!("Time elapsed INSIDE start & end index for delimiter: {:.8} seconds", start.elapsed().as_secs_f64());
+
                             let file_start_index = start_index + start_delimiter_length;
                             let file_end_index = end_index + end_delimiter_length;
 
                             let file_content = &overall_buffer[file_start_index..end_index];
                             let (_, extension) = metadata.file_name.split_at(metadata.file_name.rfind(".").unwrap_or(0));
 
-                            // if let Ok(mut file) = File::create(format!("file{}", extension)).await {
-                            //     file.write_all(&file_content).await.context("Failed to write file content")
-                            //         .expect("Failed to write file content.");
-                            // } else {
-                            //     println!("Failed to create file");
-                            // }
-
+                            println!("Time elapsed to save file: {:.8} seconds", start.elapsed().as_secs_f64());
                             if let Err(e) = Self::write_file_async(format!("file{}", extension), file_content.to_vec()).await {
                                 println!("Failed to write file: {}", e);
                             }
 
+                            println!("Time elapsed after saving file: {:.8} seconds", start.elapsed().as_secs_f64());
                             let hash_message_content = &overall_buffer[file_end_index..];
+                            let hash_length = hash_message_content.len() - end_hash_length;
+                            let hash_message_content = &hash_message_content[..hash_length];
+
                             match serde_json::from_slice::<HashMessage>(&hash_message_content) {
                                 Ok(hash_message) => {
                                     let original_hash = hash_message.hash;
@@ -160,6 +204,12 @@ impl SecureTransfer {
                                     println!("Failed to deserialize hash message: {}", e);
                                 }
                             }
+
+                            println!("Time elapsed before sending ack: {:.8} seconds", start.elapsed().as_secs_f64());
+                            if let Err(e) = Self::send_acknowledgment(&mut socket, "ACK_RECEIVED").await {
+                                println!("Error sending acknowledgment: {}", e);
+                            }
+                            println!("Time elapsed after sending ack: {:.8} seconds", start.elapsed().as_secs_f64());
                         }
                     }
                 }
@@ -216,25 +266,27 @@ impl SecureTransfer {
         let metadata_length = metadata.len() as u64;
         stream.write_all(&metadata_length.to_be_bytes()).await.context("Failed to write metadata length")?;
         stream.write_all(&metadata.as_bytes()).await.context("Failed to write metadata")?;
+        stream.flush().await?;
         println!("Metadata sent: {}", metadata);
-        // tokio::time::sleep(std::time::Duration::from_secs(2)).await;
-        // println!("Waited 2 seconds");
-        // if let Err(err) = Self::wait_for_acknowledgment(stream).await {
-        //     println!("Error waiting for acknowledgment: {}", err);
-        // }
         Ok(())
     }
 
     pub async fn send_acknowledgment(stream: &mut TcpStream, message: &str) -> Result<()> {
         stream.write_all(message.as_bytes()).await.context("Failed to send acknowledgment")?;
+        stream.flush().await?;
         Ok(())
     }
 
     pub async fn wait_for_acknowledgment(stream: &mut TcpStream) -> Result<()> {
-        let mut buffer = [0; 1024];
-        let bytes = stream.read(&mut buffer).await?;
-        let message = from_utf8(&buffer[..bytes])?;
-        println!("Received ACK: {}", message);
+        let mut ack_buffer = [0; 1024];
+        let ack_size = stream.read(&mut ack_buffer).await.context("Failed to read acknowledgment from receiver")?;
+        let ack_message = from_utf8(&ack_buffer[..ack_size]).context("Failed to decode acknowledgment message")?;
+
+        if ack_message == "ACK_RECEIVED" {
+            println!("Acknowledgment received from receiver: {}", ack_message);
+        } else {
+            println!("Unexpected message received as acknowledgment: {}", ack_message);
+        }
         Ok(())
     }
 }
